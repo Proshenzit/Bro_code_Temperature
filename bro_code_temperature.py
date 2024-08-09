d9073a33140468ccc777954393d87e614c4d8822
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-08 23:11:06 -0700

    Add files via upload

---
+++ bro_code_temperature.py
@@ -0,0 +1,9 @@
+temp  = int (input(" what is  the teparature outside: "))
+
+if not(temp>=0 and temp<=30):
+    print("the temperature  is bad today ")
+    print("stay  inside")
+
+elif not (temp<0 or temp>30):
+    print("the temperature  is good today ")
+    print("go outside! ")
